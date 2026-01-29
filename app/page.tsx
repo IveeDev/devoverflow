47eb9ac862a323d2b2b6c608e2bcee023c36a900
Innocent Idoko <iviidev899@gmail.com>
2026-01-29 06:26:33 +0100

    Implement Asset and Meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Welcome To The Home Page</div>;
+  return (
+    <div>
+      <h1 className="h1-bold font-normal">Welcome To The Home Page</h1>
+      <h1 className="text-2xl font-space-grotesk">Welcome To The Home</h1>
+    </div>
+  );
 };
 
 export default Home;
